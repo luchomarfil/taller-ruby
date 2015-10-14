92ebb0d3f2aa705d536dae50468d1fab75d598fe
luchomarfil <luchomarfil@gmail.com>
2015-10-14 20:27:48 -0300

    metodos resumidos en un archivo para usarlos con el minitest/spec

---
+++ practica3_test3.rb
@@ -0,0 +1,64 @@
+def en_palabras(theTime)
+	case theTime.min
+	when 0..10
+		hora = "Son las #{theTime.hour} en punto"
+	when 11..20
+		hora =  "Son las #{theTime.hour} y cuarto"
+	when 21..34
+		hora =  "Son las #{theTime.hour} y media"
+	when 35..44
+		hora = "Son las #{theTime.hour + 1} menos veinticinco"
+	when 45..55
+		hora = "Son las #{theTime.hour + 1} menos cuarto"
+	when 56..59
+		hora = "casi las #{theTime.hour + 1}"
+	end
+end
+
+def contar(frase, buscada)
+	frase.scan(/#{buscada}/).count
+end
+
+def contar_palabras(frase, buscada)
+	frase.scan(/\b#{buscada}\b/).count
+end
+
+def longitud(arreglo)
+	arreglo.map { |e| e.size }
+end
+
+def ordenar_arreglo(arr)
+    arr.sort
+end
+
+def ordenar(*valores)
+    valores.sort
+end
+
+def longitud(*params)
+  params.each do |variable|
+      puts "#{variable.to_s} --> #{variable.to_s.size}"
+  end
+end
+
+module Oposite
+  def opposite
+    !self
+  end
+
+end
+
+class TrueClass
+  include Oposite
+end
+class FalseClass
+  include Oposite
+end
+
+
+def da_nil?
+  yield == nil
+end
+
+puts da_nil? {}
+puts da_nil? {"Algo distinto de nil"}
